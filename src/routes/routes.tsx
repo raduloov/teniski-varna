9753311876606b4f709e736bdea0452f632c9149
Yavor Radulov <raduloov@gmail.com>
2022-12-01 21:28:57 +0200

    setup project

---
+++ src/routes/routes.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+export const AppRoutes = () => {
+  return (
+    <>
+      <Routes>
+        {/* <Route path="/" element={<HomePage />} /> */}
+        {/* <Route path="/details" element={<DetailsPage />} /> */}
+        {/* <Route path="/admin-panel" element={<AdminPanelPage />} /> */}
+        {/* <Route path="/about" element={<AboutPage />} /> */}
+        {/* <Route path="/contact" element={<ContactPage />} /> */}
+      </Routes>
+    </>
+  );
+};
