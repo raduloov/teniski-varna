bb239d04ac279fe95ad747bfa5fe50ff690ba507
Yavor Radulov <raduloov@gmail.com>
2022-12-04 22:49:42 +0200

    deploy on vercel

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -4,14 +4,24 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyC-CehY2KTdOVerlTLSJ2PU8Xcx9T-gR5A',
-  authDomain: 'teniski-varna.firebaseapp.com',
-  projectId: 'teniski-varna',
-  storageBucket: 'teniski-varna.appspot.com',
-  messagingSenderId: '756720586054',
-  appId: '1:756720586054:web:85c28ff48c874cf95ea947',
-  measurementId: 'G-4KKJN85TH7'
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_PROJECT_ID,
+  projectId: process.env.FIREBASE_STORAGE_BUCKET,
+  storageBucket: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  messagingSenderId: process.env.FIREBASE_APP_ID,
+  appId: process.env.FIREBASE_MEASUREMENT_ID,
+  measurementId: process.env.FIREBASE_KEY
 };
+
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyC-CehY2KTdOVerlTLSJ2PU8Xcx9T-gR5A',
+//   authDomain: 'teniski-varna.firebaseapp.com',
+//   projectId: 'teniski-varna',
+//   storageBucket: 'teniski-varna.appspot.com',
+//   messagingSenderId: '756720586054',
+//   appId: '1:756720586054:web:85c28ff48c874cf95ea947',
+//   measurementId: 'G-4KKJN85TH7'
+// };
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,19 +1,40 @@
 import React, { useEffect, useState } from 'react';
 import { HomeContainer } from '../containers/HomeContainer';
 import { collection, getDocs } from 'firebase/firestore';
-import { db } from '../firebase/firebaseConfig';
+import { db, storage } from '../firebase/firebaseConfig';
+import { ref, getDownloadURL, listAll } from 'firebase/storage';
 
 export const HomePage = () => {
   const [products, setProducts] = useState<any>([]); // TODO Yavor: Create type for products
   const productCollectionRef = collection(db, 'products');
 
-  useEffect(() => {
-    const getProducts = async () => {
-      const data = await getDocs(productCollectionRef);
-      setProducts(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-    };
+  const getProducts = async () => {
+    const data = await getDocs(productCollectionRef);
+    const products = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
 
+    products.forEach((product: any) => {
+      const imageRef = ref(storage, `images/${product.image}`);
+      getDownloadURL(imageRef).then((imageUrl) => console.log(imageUrl));
+    });
+
+    setProducts(products);
+  };
+
+  // const getImages = () => {
+  //   const imagesListRef = ref(storage, 'images/');
+
+  //   listAll(imagesListRef).then((response) => {
+  //     response.items.forEach((item) => {
+  //       getDownloadURL(item).then((url) => {
+  //         setImages((prev) => [...prev, url]);
+  //       });
+  //     });
+  //   });
+  // };
+
+  useEffect(() => {
     getProducts();
+    // getImages();
   }, []);
 
   return <HomeContainer products={products} />;
