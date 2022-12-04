153825978d8459616925a11a9dd0b851b85bc89a
Yavor Radulov <raduloov@gmail.com>
2022-12-04 23:22:32 +0200

    deploy on vercel

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -4,13 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: `${process.env.FIREBASE_KEY}`,
-  authDomain: `${process.env.FIREBASE_AUTH_DOMAIN}`,
-  projectId: `${process.env.FIREBASE_PROJECT_ID}`,
-  storageBucket: `${process.env.FIREBASE_STORAGE_BUCKET}`,
-  messagingSenderId: `${process.env.FIREBASE_MESSAGING_SENDER_ID}`,
-  appId: `${process.env.FIREBASE_APP_ID}`,
-  measurementId: `${process.env.FIREBASE_MEASUREMENT_ID}`
+  apiKey: `${process.env.VERCEL_URL_FIREBASE_KEY}`,
+  authDomain: `${process.env.VERCEL_URL_FIREBASE_AUTH_DOMAIN}`,
+  projectId: `${process.env.VERCEL_URL_FIREBASE_PROJECT_ID}`,
+  storageBucket: `${process.env.VERCEL_URL_FIREBASE_STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.VERCEL_URL_FIREBASE_MESSAGING_SENDER_ID}`,
+  appId: `${process.env.VERCEL_URL_FIREBASE_APP_ID}`,
+  measurementId: `${process.env.VERCEL_URL_FIREBASE_MEASUREMENT_ID}`
 };
 
 console.log('firebaseConfig:', firebaseConfig);
