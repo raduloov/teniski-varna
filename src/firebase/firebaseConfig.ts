2ad98ff5f25b9ae1c6aa19692e51cc2194999d0f
Yavor Radulov <raduloov@gmail.com>
2022-12-04 22:54:53 +0200

    deploy on vercel

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -4,13 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_PROJECT_ID,
-  projectId: process.env.FIREBASE_STORAGE_BUCKET,
-  storageBucket: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  messagingSenderId: process.env.FIREBASE_APP_ID,
-  appId: process.env.FIREBASE_MEASUREMENT_ID,
-  measurementId: process.env.FIREBASE_KEY
+  apiKey: `${process.env.FIREBASE_API_KEY}`,
+  authDomain: `${process.env.FIREBASE_PROJECT_ID}`,
+  projectId: `${process.env.FIREBASE_STORAGE_BUCKET}`,
+  storageBucket: `${process.env.FIREBASE_MESSAGING_SENDER_ID}`,
+  messagingSenderId: `${process.env.FIREBASE_APP_ID}`,
+  appId: `${process.env.FIREBASE_MEASUREMENT_ID}`,
+  measurementId: `${process.env.FIREBASE_KEY}`
 };
 
 // const firebaseConfig = {
