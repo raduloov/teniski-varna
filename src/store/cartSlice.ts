ee9714370cc311e57478c24b9781d377fead07e7
Yavor Radulov <raduloov@gmail.com>
2022-12-23 15:32:46 +0200

    make cart work with local storage

--- src/store/cartSlice.ts
+++ src/store/cartSlice.ts
@@ -8,10 +8,22 @@ export type InitialState = Array<CartProduct>;
 
 const initialState: InitialState = [];
 
+const getLocalItems = (): CartProduct[] =>
+  JSON.parse(localStorage.getItem('cartItems') ?? '[]');
+
 export const cartSlice = createSlice({
   name: 'cart',
   initialState,
   reducers: {
+    fillCartFromLocalStorage: (state) => {
+      const localItems = getLocalItems();
+
+      localItems.forEach((localItem) => {
+        if (!state.find((item) => item.id === localItem.id)) {
+          state.push(localItem);
+        }
+      });
+    },
     addToCart: (state, { payload }) => {
       const product: CartProduct = mapProductToCartProduct(
         payload.product,
@@ -19,25 +31,45 @@ export const cartSlice = createSlice({
         payload.selectedSize
       );
 
-      if (!state.find((item) => item.id === product.id)) {
+      const localItems = getLocalItems();
+      const itemExistsInState = state.find((item) => item.id === product.id);
+      const itemExistsInLocalStorage = localItems.find(
+        (item) => item.id === product.id
+      );
+
+      if (!itemExistsInState && !itemExistsInLocalStorage) {
         state.push(product);
+        localItems.push(product);
+        localStorage.setItem('cartItems', JSON.stringify(localItems));
       } else {
-        const index = state.findIndex((item) => item.id === product.id);
-        state[index].quantity++;
+        const stateIndex = state.findIndex((item) => item.id === product.id);
+        state[stateIndex].quantity++;
+        const localIndex = localItems.findIndex(
+          (item) => item.id === product.id
+        );
+        localItems[localIndex].quantity++;
+        localStorage.setItem('cartItems', JSON.stringify(localItems));
       }
     },
     removeFromCart: (state, { payload }) => {
-      const index = state.findIndex(
+      const localItems = getLocalItems();
+      const stateIndex = state.findIndex(
         (product) => product.id === payload.product.id
       );
-      const currentQuantity = state.find(
+      const currentStateQuantity = state[stateIndex].quantity;
+      const localIndex = localItems.findIndex(
         (product) => product.id === payload.product.id
-      )?.quantity;
+      );
+      const currentLocalQuantity = localItems[localIndex].quantity;
 
-      if (currentQuantity === 1) {
-        state.splice(index, 1);
+      if (currentStateQuantity === 1 && currentLocalQuantity === 1) {
+        state.splice(stateIndex, 1);
+        localItems.splice(localIndex, 1);
+        localStorage.setItem('cartItems', JSON.stringify(localItems));
       } else {
-        state[index].quantity--;
+        state[stateIndex].quantity--;
+        localItems[localIndex].quantity--;
+        localStorage.setItem('cartItems', JSON.stringify(localItems));
       }
     }
   }
