5f905e965159c00e4f218a3be06a085d931506d5
gbogomilov <gbogomilov@gmail.com>
2024-01-27 20:22:58 +0200

    added logic to hide header from certain paths

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,23 @@ import 'react-toastify/dist/ReactToastify.css';
 import { toast, ToastContainer } from 'react-toastify';
 import { AppRoutes } from './routes/routes';
 import { Header } from './components/features/home/Header';
+import { useLocation } from 'react-router';
+import { excludeHeader } from './utils/excludeHeader';
 
 export const App = () => {
   const [topNavigationShow, setTopNavigationShow] = useState<boolean>(false);
+  const location = useLocation();
+  const currentPath = location.pathname;
+
   return (
     <>
       <ToastContainer position={toast.POSITION.BOTTOM_CENTER} />
-      <Header
-        setTopNavigationShow={setTopNavigationShow}
-        topNavigationShow={topNavigationShow}
-      />
+      {excludeHeader(currentPath) && (
+        <Header
+          setTopNavigationShow={setTopNavigationShow}
+          topNavigationShow={topNavigationShow}
+        />
+      )}
       <AppRoutes />
     </>
   );
