e68f572dbb261e46dd5c8ce85d75afc8cd888000
gbogomilov <gbogomilov@gmail.com>
2024-09-10 19:24:53 +0300

    testing payment

--- src/pages/CheckoutPage.tsx
+++ src/pages/CheckoutPage.tsx
@@ -1,59 +1,52 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import * as MyPOSEmbedded from 'mypos-embedded-checkout';
 
 export const CheckoutPage = () => {
-  // console.log(MyPOSEmbedded);
-  // const paymentParams = {
-  //   sid: '000000000000010',
-  //   walletNumber: '61938166610',
-  //   amount: 23.45,
-  //   currency: 'EUR',
-  //   orderID: Math.random().toString(36).substr(2, 9),
-  //   urlNotify: MyPOSEmbedded.IPC_URL + '/client/ipcNotify',
-  //   urlOk: window.location.href,
-  //   urlCancel: window.location.href,
-  //   keyIndex: 1,
-  //   cartItems: [
-  //     {
-  //       article: 'HP ProBook 6360b sticker',
-  //       quantity: 2,
-  //       price: 10,
-  //       currency: 'EUR'
-  //     },
-  //     {
-  //       article: 'Delivery',
-  //       quantity: 1,
-  //       price: 3.45,
-  //       currency: 'EUR'
-  //     }
-  //   ]
-  // };
+  const paymentParams = {
+    sid: '768323',
+    ipcLanguage: 'bg',
+    walletNumber: '40559548405',
+    amount: 23.45,
+    currency: 'BGN',
+    orderID: Math.random().toString(36).substr(2, 9),
+    urlNotify: MyPOSEmbedded.IPC_URL + '/client/ipcNotify',
+    urlOk: window.location.href,
+    urlCancel: window.location.href,
+    keyIndex: 1,
+    cartItems: [
+      {
+        article: 'HP ProBook 6360b sticker',
+        quantity: 2,
+        price: 1,
+        currency: 'BGN'
+      }
+    ]
+  };
 
-  // const callbackParams = {
-  //   isSandbox: true,
-  //   onSuccess: function (data: any) {
-  //     console.log('success callback');
-  //     console.log(data);
-  //     document!.getElementById(
-  //       'paymentDetails'
-  //     )!.innerHTML = `Order ID:<br/>${data['OrderID']}<br/><br/>Reference:<br/>${data['IPC_Trnref']}<br/><br/>Date:<br/>${data['RequestDateTime']}<br/><br/>STAN:<br/>${data['RequestSTAN']}`;
-  //   },
-  //   onError: function () {
-  //     console.log('error');
-  //     alert('An error occured');
-  //   }
-  // };
-  // useEffect(() => {
-  //   MyPOSEmbedded.createPayment(
-  //     'myPOSEmbeddedCheckout',
-  //     paymentParams,
-  //     callbackParams
-  //   );
-  // }, []);
-  //TODO - integrate myPos payment
+  const callbackParams = {
+    isSandbox: true,
+    onSuccess: function (data: any) {
+      console.log('success callback');
+      console.log(data);
+    },
+
+    onError: function () {
+      console.log('error');
+    }
+  };
+
+  useEffect(() => {
+    MyPOSEmbedded.createPayment(
+      'embeddedCheckout',
+      paymentParams,
+      callbackParams
+    );
+  }, []);
+
   return (
     <div>
       <h1>Checkout</h1>
-      <div id="myPOSEmbeddedCheckout"></div>
+      <div id="embeddedCheckout"></div>
     </div>
   );
 };
