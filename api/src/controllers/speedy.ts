ccd54a1963722c35c5630541d8bbf79f6e3035fb
Yavor Radulov <raduloov@gmail.com>
2024-10-15 00:43:46 +0300

    send speedy credentials from client

--- api/src/controllers/speedy.ts
+++ api/src/controllers/speedy.ts
@@ -1,11 +1,11 @@
 import { Request, Response } from 'express';
 import { SPEEDY_BASE_URL } from '../config';
 
-const { SPEEDY_USERNAME, SPEEDY_PASSWORD } = process.env;
-
 export const listOffices = async (req: Request, res: Response) => {
+  const { username, password } = req.params;
+
   const response = await fetch(
-    `${SPEEDY_BASE_URL}/location/office?userName=${SPEEDY_USERNAME}&password=${SPEEDY_PASSWORD}&countryId=100`
+    `${SPEEDY_BASE_URL}/location/office?userName=${username}&password=${password}&countryId=100`
   );
   const data = await response.json();
   res.json(data);
