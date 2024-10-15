e114ac95802bd6885995ca99d77d0ea58034e734
Yavor Radulov <raduloov@gmail.com>
2024-10-15 10:46:42 +0300

    add listCities and listOfficesByCity

--- api/src/controllers/speedy.ts
+++ api/src/controllers/speedy.ts
@@ -2,10 +2,30 @@ import { Request, Response } from 'express';
 import { SPEEDY_BASE_URL } from '../config';
 
 export const listOffices = async (req: Request, res: Response) => {
-  const { username, password, cityId } = req.headers;
+  const { username, password } = req.headers;
 
   const response = await fetch(
     `${SPEEDY_BASE_URL}/location/office?userName=${username}&password=${password}&countryId=100`
+  );
+  const data = await response.json();
+  res.json(data);
+};
+
+export const listCities = async (req: Request, res: Response) => {
+  const { username, password } = req.headers;
+
+  const response = await fetch(
+    `${SPEEDY_BASE_URL}/location/site?userName=${username}&password=${password}&countryId=100`
+  );
+  const data = await response.json();
+  res.json(data);
+};
+
+export const listOfficesByCity = async (req: Request, res: Response) => {
+  const { username, password, cityName } = req.headers;
+
+  const response = await fetch(
+    `${SPEEDY_BASE_URL}/location/office/nearest-offices?userName=${username}&password=${password}&countryId=100&siteName=${cityName}`
   );
   const data = await response.json();
   res.json(data);
--- api/src/routes/speedy.ts
+++ api/src/routes/speedy.ts
@@ -1,8 +1,14 @@
 import { Router } from 'express';
-import { listOffices } from '../controllers/speedy';
+import {
+  listCities,
+  listOffices,
+  listOfficesByCity
+} from '../controllers/speedy';
 
 const router = Router();
 
 router.get('/listOffices', listOffices);
+router.get('/listCities', listCities);
+router.get('/listOfficesByCity', listOfficesByCity);
 
 export default router;
