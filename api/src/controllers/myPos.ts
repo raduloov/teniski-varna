81322a2d71f52a9f3fd58292e1ab73ad3e6a9028
Yavor Radulov <raduloov@gmail.com>
2024-10-23 11:18:14 +0300

    debug api

--- api/src/controllers/myPos.ts
+++ api/src/controllers/myPos.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from 'express';
 
 export const myPosNotify = async (req: Request, res: Response) => {
+  console.log('request:', req);
+  console.log('body:', req.body);
   res.status(200).send('OK');
 };
